8bc7552e13a7477729c30f0c3772acfec2349fa6
JiJi <i@mmdjiji.com>
2021-02-23 00:21:24 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# auto
+# 一键脚本
  
+## Ubuntu 更换清华APT源
+执行如下命令行即可:
+```sh
+sudo <(curl -sL https://cdn.jsdelivr.net/gh/mmdjiji/auto@master/ubuntu/apt-tsinghua.sh)
+```
+
+如果上面的脚本未成功执行，可尝试下面的备选方案:
+```sh
+sudo <(curl -sL https://raw.githubusercontent.com/mmdjiji/auto/master/ubuntu/apt-tsinghua.sh)
+```
---
+++ ubuntu/apt-tsinghua.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# apt-tsinghua.sh (for ubuntu)
+# Author: JiJi
+# Function: Change ubuntu apt source to tsinghua source automatically.
+# GitHub: https://github.com/mmdjiji/auto
+
+if [ `id -u` -eq 0 ];then
+	system_version=$(cat /etc/os-release | grep VERSION_CODENAME |awk -F'=' '{print $2}')
+  cp /etc/apt/sources.list /etc/apt/sources.list.bak
+  echo "\
+  # https://github.com/mmdjiji/auto
+  # 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+  deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $system_version main restricted universe multiverse
+  # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $system_version main restricted universe multiverse
+  deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $system_version-updates main restricted universe multiverse
+  # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $system_version-updates main restricted universe multiverse
+  deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $system_version-backports main restricted universe multiverse
+  # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $system_version-backports main restricted universe multiverse
+  deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $system_version-security main restricted universe multiverse
+  # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $system_version-security main restricted universe multiverse
+
+  # 预发布软件源，不建议启用
+  # deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $system_version-proposed main restricted universe multiverse
+  # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $system_version-proposed main restricted universe multiverse" > /etc/apt/sources.list
+  apt-get update
+  echo "Success!"
+else
+	echo "Error: Permission denied, you may use root to run this script."
+fi
